cc815636c7e758d6934b5a278d44e44afd67f9a2
Matahulag <matahulag@gmail.com>
2026-09-22 15:22:28 +0800

    second commit

--- db.php
+++ db.php
@@ -3,10 +3,9 @@ $host = "localhost";
 $user = "root";
 $pass = "";
 $dbname = "assessment_db";
- 
-$conn = mysqli_connect($host, $user, $pass, $dbname);
- 
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+
 if (!$conn) {
   die("Database connection failed: " . mysqli_connect_error());
 }
-?>
